354ca4e07f406db73a110a91324fd904f64d0c6a
Cesitar16 <cesarrojasramos160@gmail.com>
2025-06-10 02:40:44 -0400

    Implementación de el Model, repository, services dto, y modificación de el applicationProperties

--- src/main/java/com/edutech/roles/RolesApplication.java
+++ src/main/java/com/edutech/roles/RolesApplication.java
@@ -3,7 +3,7 @@ package com.edutech.roles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.edutech"})
 public class RolesApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/edutech/roles/repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.edutech.roles.repository;
+
+import com.edutech.roles.Models.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+}
